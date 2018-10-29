a4d4f169f2470c10b80db11d42d7a1e1fdd368e0
Qizzok <ryanjdf@gmail.com>
2018-10-29 12:04:17 -0700

    Pushing initial files

---
+++ BaseCommands.cs
@@ -0,0 +1,220 @@
+﻿using Discord.Commands;
+using Discord;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace DSBot.Modules
+{
+    public class BaseCommands : ModuleBase
+    {
+        public static readonly string[] DS = new string[]{ "ds", "ds1", "das", "das1", "dark souls", "darksouls" };
+        public static readonly string[] DSR = new string[] { "dsr", "dsre", "daremaster" };
+        public static readonly string[] DS2 = new string[] { "ds2", "das2", "dark souls 2", "dark souls ii", "darksouls2", "darksoulsii" };
+        public static readonly string[] SCHOLARS = new string[] { "sotfs", "scholar", "scholars", "dark souls 2 scholar of the first sin", "dark souls 2: scholar of the first sin", "dark souls ii scholar of the first sin", "dark souls ii: scholar of the first sin", "ds2softs", "ds2 sotfs", "das2sotfs" };
+        public static readonly string[] DS3 = new string[] { "3", "ds3", "das3", "dark souls 3", "dark souls iii", "darksouls3", "darksoulsiii" };
+        public static readonly string[] XBOX = new string[] { "xb1", "xbone", "xboxone", "xbox1", "xbox one", "one" };
+        public static readonly string[] XBOX360 = new string[] { "xbox 360", "xbox360", "360" };
+        public static readonly string[] PS3 = new string[] { "ps3", "playstation3", "playstation 3" };
+        public static readonly string[] PS4 = new string[] { "ps4", "playstation4", "playstation 4" };
+        public static readonly string[] PC = new string[] { "pc", "steam", "computer" };
+        public static readonly string[] NINTENDO = new string[] { "nintendo", "switch", "nintendo switch", "nintendoswitch" };
+        public static List<string> VOICE = new List<string>();
+
+        [Command("test", RunMode = RunMode.Async)]
+        public async Task _test()
+        {
+            await Console.Out.WriteLineAsync("Pass!");
+            await ReplyAsync("test");
+        }
+
+        [Command("bonfire", RunMode = RunMode.Async)]
+        public async Task _bonfire([Summary("Name"), Remainder()]string name = "Bonfire")
+        {
+            try
+            {
+                await Context.Message.DeleteAsync();
+                VOICE.Add(name);
+                IVoiceChannel channel = await Context.Guild.CreateVoiceChannelAsync(name);
+                IReadOnlyCollection<ICategoryChannel> col1 = await Context.Guild.GetCategoriesAsync();
+                Action<VoiceChannelProperties> action = delegate (VoiceChannelProperties x) { x.CategoryId = col1.Where(y => y.Name.ToLower().Equals("voice channels")).FirstOrDefault().Id; x.Position = (VOICE.Count.Equals(1) ? 1 : VOICE.Count - 1); };//(VOICE.Count.Equals(1) ? 1 : VOICE.Count - 1)  VOICE.Count
+                await channel.ModifyAsync(action);
+                //await _check(channel);
+            }
+            catch (Exception e)
+            {
+                await Console.Out.WriteLineAsync(e.ToString());
+            }
+        }
+
+        [Command("dswelcome", RunMode = RunMode.Async)]
+        public async Task dispatch()
+        {
+            await Context.Message.DeleteAsync();
+            EmbedBuilder builder = new EmbedBuilder()
+            {
+                Description = Program.TEXT2,
+                ThumbnailUrl = Context.Guild.IconUrl,
+                Author = new EmbedAuthorBuilder() { IconUrl = Context.Guild.IconUrl, Name = $"Welcome to the partnered Dark Souls Discord!" },
+                Color = new Color((byte)(Program._ran.Next(255)), (byte)(Program._ran.Next(255)), (byte)(Program._ran.Next(255)))
+            }.WithCurrentTimestamp();
+            IUserMessage message = await ReplyAsync(string.Empty, embed: builder.Build());
+            if (Emote.TryParse("<:XboxOne:369570227337428993>", out Emote emote1))
+            {
+                await (message as IUserMessage).AddReactionAsync(emote1);
+            }
+            if (Emote.TryParse("<:PS4:368380645858344961>", out Emote emote2))
+            {
+                await (message as IUserMessage).AddReactionAsync(emote2);
+            }
+            if (Emote.TryParse("<:Xbox360:368388223354798081>", out Emote emote3))
+            {
+                await (message as IUserMessage).AddReactionAsync(emote3);
+            }
+            if (Emote.TryParse("<:PS3:368380526253441024>", out Emote emote4))
+            {
+                await (message as IUserMessage).AddReactionAsync(emote4);
+            }
+            if (Emote.TryParse("<:PC:368373647213068300>", out Emote emote5))
+            {
+                await (message as IUserMessage).AddReactionAsync(emote5);
+            }
+            if (Emote.TryParse("<:Switch:368388237367967744>", out Emote emote6))
+            {
+                await (message as IUserMessage).AddReactionAsync(emote6);
+            }
+            if (Emote.TryParse("<:ds1:368332018838405121>", out Emote emote7))
+            {
+                await (message as IUserMessage).AddReactionAsync(emote7);
+            }
+            if (Emote.TryParse("<:tilarbiedoesabetterone:429047962728005642>", out Emote emote8))
+            {
+                await (message as IUserMessage).AddReactionAsync(emote8);
+            }
+            if (Emote.TryParse("<:ds2:368334686319017985>", out Emote emote9))
+            {
+                await (message as IUserMessage).AddReactionAsync(emote9);
+            }
+            if (Emote.TryParse("<:ds2sotfs:368335262377312266>", out Emote emote10))
+            {
+                await (message as IUserMessage).AddReactionAsync(emote10);
+            }
+            if (Emote.TryParse("<:ds3:368334983250837504>", out Emote emote11))
+            {
+                await (message as IUserMessage).AddReactionAsync(emote11);
+            }
+        }
+
+        [Command("platform", RunMode = RunMode.Async), Summary("Assigns a platform!")]
+        public async Task Platform([Summary("Role")]string role)
+        {
+            try
+            {
+                if (XBOX.Contains(role.ToLower()))
+                {
+                    await (Context.User as IGuildUser).AddRoleAsync(Context.Guild.Roles.Where(y => y.Id.Equals(194775666976227328)).Single());
+                    await ReplyAsync($"`{FirstCharToUpper(role)} role assigned!`");
+                }
+                else if (XBOX360.Contains(role.ToLower()))
+                {
+                    await (Context.User as IGuildUser).AddRoleAsync(Context.Guild.Roles.Where(y => y.Id.Equals(368856344188682240)).Single());
+                    await ReplyAsync($"`{FirstCharToUpper(role)} role assigned!`");
+                }
+                else if (PS3.Contains(role.ToLower()))
+                {
+                    await (Context.User as IGuildUser).AddRoleAsync(Context.Guild.Roles.Where(y => y.Id.Equals(368856640671318016)).Single());
+                    await ReplyAsync($"`{FirstCharToUpper(role)} role assigned!`");
+                }
+                else if (PS4.Contains(role.ToLower()))
+                {
+                    await (Context.User as IGuildUser).AddRoleAsync(Context.Guild.Roles.Where(y => y.Id.Equals(194775718108856320)).Single());
+                    await ReplyAsync($"`{FirstCharToUpper(role)} role assigned!`");
+                }
+                else if (PC.Contains(role.ToLower()))
+                {
+                    await (Context.User as IGuildUser).AddRoleAsync(Context.Guild.Roles.Where(y => y.Id.Equals(194775740162637824)).Single());
+                    await ReplyAsync($"`{FirstCharToUpper(role)} role assigned!`");
+                }
+                else if (NINTENDO.Contains(role.ToLower()))
+                {
+                    await (Context.User as IGuildUser).AddRoleAsync(Context.Guild.Roles.Where(y => y.Id.Equals(401013520637886466)).Single());
+                    await ReplyAsync($"`{FirstCharToUpper(role)} role assigned!`");
+                }
+                else
+                {
+                    await ReplyAsync("`Not found!`");
+                }
+            }
+            catch (Exception e)
+            {
+                await Console.Out.WriteLineAsync(e.ToString());
+            }
+        }
+
+        [Command("game", RunMode = RunMode.Async), Summary("Assigns a game!")]
+        public async Task Game([Summary("Game")]string game)
+        {
+            try
+            {
+                if (DS.Contains(game.ToLower()))
+                {
+                    await (Context.User as IGuildUser).AddRoleAsync(Context.Guild.Roles.Where(y => y.Id.Equals(368859767084417024)).Single());
+                    await ReplyAsync($"`{FirstCharToUpper(game)} role assigned!`");
+                }
+                else if (DS2.Contains(game.ToLower()))
+                {
+                    await (Context.User as IGuildUser).AddRoleAsync(Context.Guild.Roles.Where(y => y.Id.Equals(368857012106166275)).Single());
+                    await ReplyAsync($"`{FirstCharToUpper(game)} role assigned!`");
+                }
+                else if (SCHOLARS.Contains(game.ToLower()))
+                {
+                    await (Context.User as IGuildUser).AddRoleAsync(Context.Guild.Roles.Where(y => y.Id.Equals(368859851293589504)).Single());
+                    await ReplyAsync($"`{FirstCharToUpper(game)} role assigned!`");
+                }
+                else if (DS3.Contains(game.ToLower()))
+                {
+                    await (Context.User as IGuildUser).AddRoleAsync(Context.Guild.Roles.Where(y => y.Id.Equals(368854325516042241)).Single());
+                    await ReplyAsync($"`{FirstCharToUpper(game)} role assigned!`");
+                }
+                else if (DSR.Contains(game.ToLower()))
+                {
+                    await (Context.User as IGuildUser).AddRoleAsync(Context.Guild.Roles.Where(y => y.Id.Equals(428253711857483776)).Single());
+                    await ReplyAsync($"`{FirstCharToUpper(game)} role assigned!`");
+                }
+                else
+                {
+                    await ReplyAsync("`Not found!`");
+                }
+            }
+            catch (Exception e)
+            {
+                await Console.Out.WriteLineAsync(e.ToString());
+            }
+        }
+
+        [Command("username", RunMode = RunMode.Async), Summary("Assigns a username!"), RequireOwner()]
+        public async Task Name([Summary("Name")]string name)
+        {
+            Action<SelfUserProperties> action = delegate (SelfUserProperties x) { x.Username = name; };
+            await Program._client.CurrentUser.ModifyAsync(action);
+        }
+
+        private async Task _check(IVoiceChannel channel)
+        {
+            await Task.Delay(10000);
+            while (true)
+            {
+                if (await channel.GetUsersAsync().Count() == 0)
+                {
+                    await channel.DeleteAsync(new RequestOptions() { AuditLogReason = "Empty" });
+                    break;
+                }
+                await Task.Delay(10000);
+            }
+        }
+
+        public static string FirstCharToUpper(string arg) => (String.IsNullOrEmpty(arg) ? throw new ArgumentException("ARGH!") : arg.First().ToString().ToUpper() + arg.Substring(1));
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,337 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DSBot
+{
+    public class Program
+    {
+        static void Main(string[] args) => new Program().Start(args).GetAwaiter().GetResult();
+
+        public static CommandService _commands;
+        public static DiscordSocketClient _client;
+        public static Random _ran = new Random();
+        private IServiceProvider _services;
+        public static readonly string TEXT1 = @"Brave Undead, welcome to the partnered **Dark Souls** Discord. Please, take your time to read the <#389199668111998977> channel.
+
+To see and speak in this server, perform the following actions in the <#194771543992041472> channel:
+    1) Use the `!platform` command to tag  what platform(s) you play on to gain access to matchmaking channels. e.g. `!platform PC`
+    → Available platform roles:
+        Switch
+        One
+        PS4
+        360
+        PS3
+        PC
+
+    2) Use the `!game` command to tag which Dark Souls games you play/own to gain access to that game's channels. e.g. `!game sotfs`
+    → Available videogame roles:
+        DS 
+        DSR
+        DS2
+        SotFS
+        DS3
+
+*If you come across any issue while following these steps, please contact one of our moderators*.
+
+Remember to check out <#389199668111998977> for more details on how this server works!";
+        public static readonly string TEXT2 = @"Brave Undead, welcome to the partnered **Dark Souls** Discord. Please, take your time to read the <#389199668111998977> channel.
+*If you come across any issue while following these steps, please contact one of our moderators and manually apply your roles via the* <#194771543992041472> *channel*.
+
+To see and speak in this server, please click on the following emojis to apply roles for which:
+    1) Platforms you play Dark Souls videogames on
+        Switchemote - I play on Nintendo Switch
+        One - I play on Xbox One 
+        PS4 - I play on PlayStation 4
+        360 - I play on Xbox 360
+        PS3 - I play on PlayStation 3
+        PC - I play on PC
+        Switch - I play on Nintendo Switch
+
+    2) Dark Souls videogames that you own
+        DSemote - If you own the original Dark Souls
+        DSRemote - If you own Dark Souls: Remastered
+        DS2emote - If you own the DX9 versions of Dark Souls II
+        SotFSemote If you own the DX11 version of Dark Souls II: Scholar of the First Sin
+        DS3emote - If you own Dark Souls III";
+
+        public async Task Start(string[] args)
+        {
+            await DoBotStuff();
+
+            await Task.Delay(-1);
+        }
+
+        public async Task DoBotStuff()
+        {
+            _commands = new CommandService();
+            _client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Info,
+                MessageCacheSize = 50
+                //TotalShards = 2
+            });
+            _services = new ServiceCollection().AddSingleton(_client).AddSingleton(_commands).BuildServiceProvider();
+            _client.Log += Log;
+
+            await InstallCommands();
+            ConfigureEventHandlers();
+
+            await _client.LoginAsync(TokenType.Bot, "REDACTED");
+            await _client.StartAsync();
+        }
+
+        private async Task InstallCommands()
+        {
+            _client.MessageReceived += HandleCommand;
+
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly());
+        }
+
+        private async Task HandleCommand(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+            if (message == null || arg.Author.IsBot)
+            {
+                return;
+            }
+            int argPos = 0;
+            if (!(message.HasCharPrefix('!', ref argPos) || message.HasMentionPrefix(_client.CurrentUser, ref argPos)))
+            {
+                return;
+            }
+            var context = new CommandContext(_client, message);
+            var result = await _commands.ExecuteAsync(context, argPos, _services);
+        }
+
+        public void ConfigureEventHandlers()
+        {
+            _client.UserVoiceStateUpdated += _client_UserVoiceStateUpdated;
+            _client.UserJoined += _client_UserJoined;
+            _client.ReactionAdded += _client_ReactionAdded;
+            _client.ReactionRemoved += _client_ReactionRemoved;
+        }
+
+        private async Task _client_ReactionAdded(Cacheable<IUserMessage, ulong> message, ISocketMessageChannel channel, SocketReaction reaction)
+        {
+            if (!reaction.UserId.Equals(_client.CurrentUser.Id) && channel.Id.Equals(369216525825212416)) {
+                if (Emote.TryParse("<:XboxOne:369570227337428993>", out Emote emote1)) {
+                    if (reaction.Emote.Name.Equals(emote1.Name))
+                    {
+                        await (reaction.User.Value as IGuildUser).AddRoleAsync((channel as IGuildChannel).Guild.Roles.Where(y => y.Id.Equals(194775666976227328)).Single());
+                    }
+                }
+                if (Emote.TryParse("<:PS4:368380645858344961>", out Emote emote2))
+                {
+                    if (reaction.Emote.Name.Equals(emote2.Name))
+                    {
+                        await (reaction.User.Value as IGuildUser).AddRoleAsync((channel as IGuildChannel).Guild.Roles.Where(y => y.Id.Equals(194775718108856320)).Single());
+                    }
+                }
+                if (Emote.TryParse("<:Xbox360:368388223354798081>", out Emote emote3))
+                {
+                    if (reaction.Emote.Name.Equals(emote3.Name))
+                    {
+                        await (reaction.User.Value as IGuildUser).AddRoleAsync((channel as IGuildChannel).Guild.Roles.Where(y => y.Id.Equals(368856344188682240)).Single());
+                    }
+                }
+                if (Emote.TryParse("<:PS3:368380526253441024>", out Emote emote4))
+                {
+                    if (reaction.Emote.Name.Equals(emote4.Name))
+                    {
+                        await (reaction.User.Value as IGuildUser).AddRoleAsync((channel as IGuildChannel).Guild.Roles.Where(y => y.Id.Equals(368856640671318016)).Single());
+                    }
+                }
+                if (Emote.TryParse("<:PC:368373647213068300>", out Emote emote5))
+                {
+                    if (reaction.Emote.Name.Equals(emote5.Name))
+                    {
+                        await (reaction.User.Value as IGuildUser).AddRoleAsync((channel as IGuildChannel).Guild.Roles.Where(y => y.Id.Equals(194775740162637824)).Single());
+                    }
+                }
+                if (Emote.TryParse("<:Switch:368388237367967744>", out Emote emote6))
+                {
+                    if (reaction.Emote.Name.Equals(emote6.Name))
+                    {
+                        await (reaction.User.Value as IGuildUser).AddRoleAsync((channel as IGuildChannel).Guild.Roles.Where(y => y.Id.Equals(401013520637886466)).Single());
+                    }
+                }
+                if (Emote.TryParse("<:ds1:368332018838405121>", out Emote emote7))
+                {
+                    if (reaction.Emote.Name.Equals(emote7.Name))
+                    {
+                        await (reaction.User.Value as IGuildUser).AddRoleAsync((channel as IGuildChannel).Guild.Roles.Where(y => y.Id.Equals(368859767084417024)).Single());
+                    }
+                }
+                if (Emote.TryParse("<:tilarbiedoesabetterone:429047962728005642>", out Emote emote8))
+                {
+                    if (reaction.Emote.Name.Equals(emote8.Name))
+                    {
+                        await (reaction.User.Value as IGuildUser).AddRoleAsync((channel as IGuildChannel).Guild.Roles.Where(y => y.Id.Equals(428253711857483776)).Single());
+                    }
+                }
+                if (Emote.TryParse("<:ds2:368334686319017985>", out Emote emote9))
+                {
+                    if (reaction.Emote.Name.Equals(emote9.Name))
+                    {
+                        await (reaction.User.Value as IGuildUser).AddRoleAsync((channel as IGuildChannel).Guild.Roles.Where(y => y.Id.Equals(368857012106166275)).Single());
+                    }
+                }
+                if (Emote.TryParse("<:ds2sotfs:368335262377312266>", out Emote emote10))
+                {
+                    if (reaction.Emote.Name.Equals(emote10.Name))
+                    {
+                        await (reaction.User.Value as IGuildUser).AddRoleAsync((channel as IGuildChannel).Guild.Roles.Where(y => y.Id.Equals(368859851293589504)).Single());
+                    }
+                }
+                if (Emote.TryParse("<:ds3:368334983250837504>", out Emote emote11))
+                {
+                    if (reaction.Emote.Name.Equals(emote11.Name))
+                    {
+                        await (reaction.User.Value as IGuildUser).AddRoleAsync((channel as IGuildChannel).Guild.Roles.Where(y => y.Id.Equals(368854325516042241)).Single());
+                    }
+                }
+            }
+        }
+
+        private async Task _client_ReactionRemoved(Cacheable<IUserMessage, ulong> message, ISocketMessageChannel channel, SocketReaction reaction)
+        {
+            if (channel.Id.Equals(369216525825212416))
+            {
+                if (Emote.TryParse("<:XboxOne:369570227337428993>", out Emote emote1))
+                {
+                    if (reaction.Emote.Name.Equals(emote1.Name))
+                    {
+                        await (reaction.User.Value as IGuildUser).RemoveRoleAsync((channel as IGuildChannel).Guild.Roles.Where(y => y.Id.Equals(194775666976227328)).Single());
+                    }
+                }
+                if (Emote.TryParse("<:PS4:368380645858344961>", out Emote emote2))
+                {
+                    if (reaction.Emote.Name.Equals(emote2.Name))
+                    {
+                        await (reaction.User.Value as IGuildUser).RemoveRoleAsync((channel as IGuildChannel).Guild.Roles.Where(y => y.Id.Equals(194775718108856320)).Single());
+                    }
+                }
+                if (Emote.TryParse("<:Xbox360:368388223354798081>", out Emote emote3))
+                {
+                    if (reaction.Emote.Name.Equals(emote3.Name))
+                    {
+                        await (reaction.User.Value as IGuildUser).RemoveRoleAsync((channel as IGuildChannel).Guild.Roles.Where(y => y.Id.Equals(368856344188682240)).Single());
+                    }
+                }
+                if (Emote.TryParse("<:PS3:368380526253441024>", out Emote emote4))
+                {
+                    if (reaction.Emote.Name.Equals(emote4.Name))
+                    {
+                        await (reaction.User.Value as IGuildUser).RemoveRoleAsync((channel as IGuildChannel).Guild.Roles.Where(y => y.Id.Equals(368856640671318016)).Single());
+                    }
+                }
+                if (Emote.TryParse("<:PC:368373647213068300>", out Emote emote5))
+                {
+                    if (reaction.Emote.Name.Equals(emote5.Name))
+                    {
+                        await (reaction.User.Value as IGuildUser).RemoveRoleAsync((channel as IGuildChannel).Guild.Roles.Where(y => y.Id.Equals(194775740162637824)).Single());
+                    }
+                }
+                if (Emote.TryParse("<:Switch:368388237367967744>", out Emote emote6))
+                {
+                    if (reaction.Emote.Name.Equals(emote6.Name))
+                    {
+                        await (reaction.User.Value as IGuildUser).RemoveRoleAsync((channel as IGuildChannel).Guild.Roles.Where(y => y.Id.Equals(401013520637886466)).Single());
+                    }
+                }
+                if (Emote.TryParse("<:ds1:368332018838405121>", out Emote emote7))
+                {
+                    if (reaction.Emote.Name.Equals(emote7.Name))
+                    {
+                        await (reaction.User.Value as IGuildUser).RemoveRoleAsync((channel as IGuildChannel).Guild.Roles.Where(y => y.Id.Equals(368859767084417024)).Single());
+                    }
+                }
+                if (Emote.TryParse("<:tilarbiedoesabetterone:429047962728005642>", out Emote emote8))
+                {
+                    if (reaction.Emote.Name.Equals(emote8.Name))
+                    {
+                        await (reaction.User.Value as IGuildUser).RemoveRoleAsync((channel as IGuildChannel).Guild.Roles.Where(y => y.Id.Equals(428253711857483776)).Single());
+                    }
+                }
+                if (Emote.TryParse("<:ds2:368334686319017985>", out Emote emote9))
+                {
+                    if (reaction.Emote.Name.Equals(emote9.Name))
+                    {
+                        await (reaction.User.Value as IGuildUser).RemoveRoleAsync((channel as IGuildChannel).Guild.Roles.Where(y => y.Id.Equals(368857012106166275)).Single());
+                    }
+                }
+                if (Emote.TryParse("<:ds2sotfs:368335262377312266>", out Emote emote10))
+                {
+                    if (reaction.Emote.Name.Equals(emote10.Name))
+                    {
+                        await (reaction.User.Value as IGuildUser).RemoveRoleAsync((channel as IGuildChannel).Guild.Roles.Where(y => y.Id.Equals(368859851293589504)).Single());
+                    }
+                }
+                if (Emote.TryParse("<:ds3:368334983250837504>", out Emote emote11))
+                {
+                    if (reaction.Emote.Name.Equals(emote11.Name))
+                    {
+                        await (reaction.User.Value as IGuildUser).RemoveRoleAsync((channel as IGuildChannel).Guild.Roles.Where(y => y.Id.Equals(368854325516042241)).Single());
+                    }
+                }
+            }
+        }
+
+        private async Task _client_UserJoined(SocketGuildUser arg)
+        {
+            EmbedBuilder builder = new EmbedBuilder()
+            {
+                Description = TEXT1,
+                ThumbnailUrl = (arg as SocketGuildUser).Guild.IconUrl,
+                Author = new EmbedAuthorBuilder() { IconUrl = arg.GetAvatarUrl(ImageFormat.Auto, (ushort)128), Name = $"{arg.Username} joined {arg.Guild.Name}!" },
+                Color = new Color((byte)(_ran.Next(255)), (byte)(_ran.Next(255)), (byte)(_ran.Next(255)))
+            }.WithCurrentTimestamp();
+            IDMChannel y = await (arg as SocketUser).GetOrCreateDMChannelAsync();
+            await y.SendMessageAsync(string.Empty, false, embed: builder.Build());
+        }
+
+        private async Task _client_UserVoiceStateUpdated(SocketUser arg1, SocketVoiceState arg2, SocketVoiceState arg3)
+        {
+            var vc = arg2.VoiceChannel ?? arg3.VoiceChannel;
+            if (!vc.Users.Contains(arg1 as SocketGuildUser)) {
+                if (vc.Users.Count < 1 && Modules.BaseCommands.VOICE.Contains(vc.Name))
+                {
+                    await vc.DeleteAsync();
+                    Modules.BaseCommands.VOICE.Remove(vc.Name);
+                }
+            }
+        }
+
+        private static Task Log(LogMessage arg)
+        {
+            var cc = Console.ForegroundColor;
+            switch (arg.Severity)
+            {
+                case LogSeverity.Critical:
+                case LogSeverity.Error:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case LogSeverity.Warning:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    break;
+                case LogSeverity.Info:
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                case LogSeverity.Verbose:
+                case LogSeverity.Debug:
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    break;
+            }
+            Console.WriteLine($"{DateTime.Now,-19} [{arg.Severity,8}] {arg.Source}: {arg.Message}");
+            Console.ForegroundColor = cc;
+            return Task.CompletedTask;
+        }
+    }
+}
